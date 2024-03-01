6f8edda0006f4f79eeb5449d2d85ba7aceb36c4a
Kevin Valmorbida <kevinvalmo@gmail.com>
2024-03-01 01:39:36 +0100

    playing with daisyUI and theming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   template: `
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  `,
-  styles: [],
+    <div class="join">
+      <input type="radio" name="theme-buttons"
+             class="btn theme-controller join-item" aria-label="Default"
+             checked
+             value="default"
+      />
+      <input type="radio" name="theme-buttons"
+             class="btn theme-controller join-item" aria-label="Retro"
+             value="light"
+      />
+      <input type="radio" name="theme-buttons"
+             class="btn theme-controller join-item" aria-label="Cyberpunk"
+             value="cupcake"/>
+    </div>
+  `, styles: [],
 })
 export class AppComponent {
   title = 'angular-githubpages';
